8603d8dbdbdd9cf80c53b376ec1e562f2ca0a7cb
andrerudi12 <andrerudi12@gmail.com>
2021-03-19 13:15:19 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroSection />
+    <BannerShayna />
+    <InstaSheyna/>
+    <PartnerShayna/>
+    <FooterShayna/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroSection from "@/components/HeroSection.vue";
+import BannerShayna from "@/components/BannerShayna.vue";
+import InstaSheyna from "@/components/InstaSheyna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroSection,
+    BannerShayna,
+    InstaSheyna,
+    PartnerShayna,
+    FooterShayna
+  },
+};
+</script>
